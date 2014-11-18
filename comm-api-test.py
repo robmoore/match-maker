dc9be0dc3683f76afe8af32a9aa629c5fb54a0eb
robmoore <rob.moore@gmail.com>
2014-11-18 17:44:20 -0500

    - Added manual refresh method to client to attempt to diagnose issue with token refreshes. - Changed format of token config in secrets file. - Updating readme to correct typos and markdown mistakes.

--- comm-api-test.py
+++ comm-api-test.py
@@ -6,11 +6,9 @@ from time import time
 # Create a secrets.py file in same directory with client_id and client_secret defined like
 # client_id = "<my_client_id>"
 # client_secret = "<my_secret_id>"
-# token = {
-#  'access_token': '<my_access_token>'
-#  'refresh_token': '<my_refresh_token>'
-# }
-from secrets import client_id, client_secret, token
+# access_token = "<my_access_token>"
+# refresh_token = "<my_refresh_token>"
+from secrets import client_id, client_secret, access_token, refresh_token
 
 # Uncomment for detailed oauthlib logs. Note: It doesn't appear to work. Perhaps there's a switch that's needed as weLl?
 import logging
@@ -18,35 +16,47 @@ import sys
 log = logging.getLogger('oauthlib')
 log.addHandler(logging.StreamHandler(sys.stdout))
 log.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
-# Not sure it's legitimate to put the grant_token parameter here but it's unique to refresh requests and
-# so doesn't make sense to put it in extra for every request.
-refresh_url = "https://www.hackerschool.com/oauth/token?grant_type=refresh_token"
+refresh_url = "https://www.hackerschool.com/oauth/token"
 
-def issue_request(url):
-  def token_updater(new_token):
-      token['refresh_token'] = new_token
+token = {
+    'access_token': access_token,
+    'refresh_token': refresh_token,
+    'expires_in': "-7200"
+}
 
-  # Attempting to force refresh in every instance. We could store now() + 7200 whenever we get a new access token
-  # and use this value to express the expiration but we'd need to store it somewhere.
-  token['expires_at'] = time() - 10
+def token_updater(new_token):
+  global token
+  token = new_token
+  
+def manual_token_refresh():
 
   extra = {
     'client_id': client_id,
     'client_secret': client_secret,
-    # Maybe this should go here instead of in URL (see refresh_url comment above).
-    #'grant_type': 'refresh_token'
   }
 
-  hs = OAuth2Session(client_id,
-                     token=token,
-                     auto_refresh_kwargs=extra,
-                     auto_refresh_url=refresh_url,
-                     token_updater=token_updater)
+  hs = OAuth2Session(client_id, token = token)
+  token_updater(hs.refresh_token(refresh_url, **extra))
+
+def issue_request(url):
+  # Attempting to force refresh in every instance. We could store now() + 7200 whenever we get a new access token
+  # and use this value to express the expiration but we'd need to store it somewhere.
+  #token['expires_at'] = time() - 10
+
+  extra = {
+    'client_id': client_id,
+    'client_secret': client_secret
+  }
+
+  hs = OAuth2Session(client_id, token=token, auto_refresh_kwargs=extra, auto_refresh_url=refresh_url, token_updater=token_updater)
 
   # Trigger the automatic refresh /oauth/token
   return hs.get(url)
 
 if __name__ == '__main__':
-  response = issue_request('https://www.hackerschool.com/api/v1/people/me')
-  print response.json()
+  manual_token_refresh()
+  print "Token refreshed"
+  #response = issue_request('https://www.hackerschool.com/api/v1/people/me')
+  #print response.json()
