8b3ed28e8d5a1d2b143a8182d2484815037506f5
robmoore <rob.moore@gmail.com>
2014-11-20 19:09:12 -0500

    - Created RequestHelper class to aid in requests to OAuth protected resources. - Created HackerSchoolAPI class to demo usage of RequestHelper class. - Removing old files. - Adding __init__.py file to enable local imports.

---
+++ hacker_school_api.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from request_helper import RequestHelper
+
+refresh_url = "https://www.hackerschool.com/oauth/token"
+base_url = "https://www.hackerschool.com/api/v1"
+
+class HackerSchoolAPI(object):
+  def __init__(self):
+    self.rh = RequestHelper(refresh_url, base_url)
+
+  def find_active_batches(self): 
+    # Look up batches
+    batches = self.rh.issue_request("/batches")
+    # First two batches are active
+    if len(batches) >= 2:
+      active_batches = batches[0:2]
+    else:
+      active_batches = batches
+
+    return active_batches
+
+  def find_user_by_email(self, email):
+    # find active batches
+    active_batches = self.find_active_batches()
+    # request people in batches
+    if active_batches:
+      people = self.rh.issue_request('/batches/' + repr(active_batches[0]['id']) + '/people')
+      # Add second batch if there is one
+      if len(active_batches) >= 1:
+        people.extend(self.rh.issue_request('/batches/' + repr(active_batches[1]['id']) + '/people'))
+    
+    # TODO: What happens if people is empty?
+    return [person for person in people if person['email'] == email][0]
+
+if __name__ == '__main__':
+  hs_api = HackerSchoolAPI()
+  response = hs_api.find_active_batches()
+  print response
+  response = hs_api.find_user_by_email('rob.moore@gmail.com')
+  print response
---
+++ request_helper.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+from requests_oauthlib import OAuth2Session
+from time import time
+from urlparse import urlparse
+
+import ConfigParser
+
+# Uncomment for detailed oauthlib logs. 
+#import logging
+#import sys
+#log = logging.getLogger('oauthlib')
+#log.addHandler(logging.StreamHandler(sys.stdout))
+#log.setLevel(logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
+
+# Create a config file (see name below) in the same directory with the following format:
+#
+# [hostname]
+# client_id = <my_client_id>
+# client_secret = <my_secret_id>
+# access_token = <my_access_token>
+# refresh_token = <my_refresh_token>
+# expires_at = 0
+
+class RequestHelper(object):
+  config_file = 'RequestHelper.cfg'
+
+  def __init__(self, refresh_url, base_url):
+    self.refresh_url = refresh_url
+    self.base_url = base_url
+    self.hostname = urlparse(refresh_url).hostname
+    self.config = self.lookup_config()
+    self.token = self.make_token()
+
+  def lookup_config(self):
+    config = ConfigParser.ConfigParser()
+    config.read(self.config_file)
+
+    client_id = config.get(self.hostname, 'client_id')
+    client_secret = config.get(self.hostname, 'client_secret')
+    access_token = config.get(self.hostname, 'access_token')
+    refresh_token = config.get(self.hostname, 'refresh_token')
+    expires_at = config.getint(self.hostname, 'expires_at')
+
+    config = {
+      'client_id': client_id,
+      'client_secret': client_secret,
+      'access_token': access_token,
+      'refresh_token': refresh_token,
+      'expires_at': expires_at
+    }
+
+    return config
+    
+  def make_token(self):
+    token = {
+      'access_token': self.config['access_token'],
+      'refresh_token': self.config['refresh_token'],
+      'expires_at': self.config['expires_at']
+    }
+
+    return token
+
+  def token_updater(self, new_token):
+    print "Updating token"
+    self.token = new_token
+    self.config.set(self.hostname, 'access_token', token['access_token'])
+    self.config.set(self.hostname, 'refresh_token', token['refresh_token'])
+    self.config.set(self.hostname, 'expires_at', int(token['expires_at']))
+    with open(self.config_file, 'wb') as configfile:
+      config.write(configfile)
+
+  def issue_request(self, path):
+    # Attempting to force refresh in every instance. We could store now() + 7200 whenever we get a new access token
+    # and use this value to express the expiration but we'd need to store it somewhere.
+
+    extra = {
+      'client_id': self.config['client_id'],
+      'client_secret': self.config['client_secret']
+    }
+
+    oa2s = OAuth2Session(self.config['client_id'], 
+      token = self.token, 
+      auto_refresh_kwargs = extra, 
+      auto_refresh_url = self.refresh_url, 
+      token_updater = self.token_updater)
+
+    # Trigger the automatic refresh /oauth/token
+    return oa2s.get(self.base_url + path).json()
+
+if __name__ == '__main__':
+  rh = RequestHelper("https://www.hackerschool.com/oauth/token", "https://www.hackerschool.com/api/v1")
+  response = rh.issue_request('/people/me')
+  print response
